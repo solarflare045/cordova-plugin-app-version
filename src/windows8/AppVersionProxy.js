7066a9efa3b93269e85826e90d64c01f402507f8
Max Sorensen <max.sorensen@myriad-it.com>
2015-08-18 10:25:17 +1000

    Improved Windows support.

--- src/windows8/AppVersionProxy.js
+++ src/windows8/AppVersionProxy.js
@@ -1,7 +1,19 @@
 AppVersionProxy = {
-  getVersionNumber: function (successCallback, failCallback, args) {
+  getVersionNumber: function(successCallback, failCallback, args) {
     var version = Windows.ApplicationModel.Package.current.id.version;
-    successCallback([version.major, version.minor, version.build, version.revision].join('.'));
-  }  
+    successCallback([version.major, version.minor, version.build].join('.'));
+  },
+  getVersionCode: function(successCallback, failCallback, args) {
+    var version = Windows.ApplicationModel.Package.current.id.version;
+    successCallback(version.revision);
+  },
+  getAppName: function(successCallback, failCallback, args) {
+    var name = Windows.ApplicationModel.Package.current.displayName;
+    successCallback(name);
+  },
+  getPackageName: function(successCallback, failCallback, args) {
+    var id = Windows.ApplicationModel.Package.current.id.name;
+    successCallback(id);
+  }
 };
 cordova.commandProxy.add("AppVersion", AppVersionProxy);
